dfb72ed054aeeabdaea8f1f45a92e48f2a9ca425
小坪　均 <kotsuboon@gmail.com>
2021-01-10 20:39:09 +0900

    create: eofを利用したページ作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+
+<div class="sidevar">
+    <ul>
+        <li>
+            <a href="">トップ</a>
+        </li>
+        <li>
+            <a href="">映画出演</a>
+        </li>
+        <li>
+            <a href="">ドラマ出演</a>
+        </li>
+        <li>
+            <a href="">写真集</a>
+        </li>
+    </ul>
+</div>
+
+<div class="main">
+<h1>小坪均のホームページ</h1>
+    <div class="left-profile">
+        <img src="" alt="">
+        <p>小坪均(こつぼひとし)<br>生年月日　1989年10月19日 <br> 血液型 B型 <br> <a href="">プロフィール</a> </p>
+    </div>
+    <div class="right-profile">
+        <p>★★★ 最新情報 ★★★</p>
+    </div>
+</div>
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$html = <<<EOF
+<html>
+    <body>
+        <h1>EOFを利用したhtml</h1>
+        <p>サンプルで作ったページです。</p>
+    </body>
+</html>
+EOF;
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('hello', function (){
+    return '<html><body><h1>hello</h1></body></html>';
+});
+
+Route::get('eof', function() use($html){
+    return $html;
 });
